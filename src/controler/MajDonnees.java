983f449bba562e38a3b045279218f3606cbbeeff
Daniel <taurloc@gmail.com>
2020-06-06 05:38:47 +0200

    Ajout methode de suppression de groupe, salle ou enseignant d'une seance

--- src/controler/MajDonnees.java
+++ src/controler/MajDonnees.java
@@ -10,7 +10,6 @@ import java.time.LocalTime;
 public class MajDonnees {
 
     private SeanceDaoServiceImpl seanceDao;
-    private EnseignantDaoServiceImpl profDao;
     private SeanceEnseignantsDaoServiceImpl seDao;
     private SeanceGroupeDaoServiceImpl sgDao;
     private SeanceSalleDaoServiceImpl ssDao;
@@ -18,7 +17,6 @@ public class MajDonnees {
 
     public MajDonnees() {
         this.seanceDao = new SeanceDaoServiceImpl();
-        this.profDao = new EnseignantDaoServiceImpl();
         this.seDao = new SeanceEnseignantsDaoServiceImpl();
         this.sgDao = new SeanceGroupeDaoServiceImpl();
         this.ssDao = new SeanceSalleDaoServiceImpl();
@@ -30,6 +28,16 @@ public class MajDonnees {
      */
     public void addEnseignantToSeance(final Enseignant prof, Seance seance) {
 
+        /** TEST A FAIRE **/
+
+        Seance_Enseignants se = new Seance_Enseignants();
+        se.setProf(prof);
+        se.setSeance(seance);
+        try {
+            seDao.ajouter(se);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     /**
@@ -38,6 +46,39 @@ public class MajDonnees {
      * d’un autre groupe. Remarque : l’effectif de chaque groupe peut se comptabiliser à partir des étudiants affectés au
      * groupe.
      */
+    public void addGroupeToSeance(final Groupe grp, Seance seance) {
+
+        /** TEST A FAIRE **/
+
+        Seance_Groupes sg = new Seance_Groupes();
+        sg.setSeance(seance);
+        sg.setGroupe(grp);
+        try {
+            sgDao.ajouter(sg);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Affecter une salle à une séance de cours à condition qu’elle soit disponible sur le créneau de la séance et que sa
+     * capacité soit suffisante pour le(s) groupe(s).
+     * @param seance Seance a modifier.
+     * @param salle Salle a rajouter a la seance.
+     */
+    public void addSalleToSeance(Seance seance, final Salle salle) {
+
+        /** TEST A FAIRE **/
+
+        Seance_Salles ss = new Seance_Salles();
+        ss.setSeance(seance);
+        ss.setSalle(salle);
+        try {
+            ssDao.ajouter(ss);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
     /**
      * Modifier le cours (son nom ou son type) dans une séance de cours.
@@ -54,16 +95,6 @@ public class MajDonnees {
     }
 
     /**
-     * Affecter une salle à une séance de cours à condition qu’elle soit disponible sur le créneau de la séance et que sa
-     * capacité soit suffisante pour le(s) groupe(s).
-     * @param seance Seance a modifier.
-     * @param salle Salle a rajouter a la seance.
-     */
-    public void addSalleToSeance(Seance seance, final Salle salle) {
-
-    }
-
-    /**
      * Déplacer une séance de cours vers un autre créneau, sauf le samedi et dimanche et aux horaires d’ouverture de
      * l’école, dans une salle libre durant ce créneau et de capacité suffisante pour le(s) groupe(s), en fonction de la
      * disponibilité de l’enseignant et du (des) groupe(s) sur ce créneau.
@@ -71,14 +102,19 @@ public class MajDonnees {
      * @param heure_debut Nouvelles heure de début de la seance.
      * @param heure_fin Nouvelle heure de fin de la séance.
      * @param jour Nouvelle date de la seance.
-     * @throws SQLException Erreur de requete.
-     * @throws ClassNotFoundException Erreur de driver.
      */
-    public void changeCreneau(Seance seance, LocalTime heure_debut, LocalTime heure_fin, LocalDate jour) throws SQLException, ClassNotFoundException {
+    public void changeCreneau(Seance seance, LocalTime heure_debut, LocalTime heure_fin, LocalDate jour) {
+
+        /** TEST A FAIRE **/
+
         seance.setHeure_debut(heure_debut);
         seance.setHeure_fin(heure_fin);
         seance.setJour(jour);
-        seanceDao.modifier(seance);
+        try {
+            seanceDao.modifier(seance);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 
@@ -89,28 +125,24 @@ public class MajDonnees {
      * l’état (« en cours de validation » ou « validé »), le cours et le type de cours. Remarque : pour la même séance, il ne
      * peut pas y avoir de doublon de groupe, d’enseignant et de salle.
      */
+    public void creerNouvelleSeance() {
 
-    /**
-     *  Ajouter un enseignant à une séance de cours à condition que l’enseignant ne soit pas déjà affecté à cette séance et
-     *  qu’il soit disponible sur le créneau de la séance
-     */
+    }
 
-
-    /**
-     * Ajouter un groupe à une séance de cours à condition que le groupe ne soit pas déjà affecté à cette séance, qu’il soit
-     * disponible sur le créneau de la séance et que la salle soit de capacité suffisante pour le(s) groupe(s)
-     */
 
     /**
      * Annuler une séance de cours (changer l’état de cette séance à « annulée »).
      * @param seance Seance a annuler.
-     * @throws SQLException Probleme requete.
-     * @throws ClassNotFoundException Probleme driver.
      */
-    public void annulerSeance(Seance seance) throws SQLException, ClassNotFoundException {
+    public void annulerSeance(Seance seance) {
         if (seance.getEtat() != 0) {
             seance.setEtat(0);
-            seanceDao.modifier(seance);
+            try {
+                seanceDao.modifier(seance);
+                System.out.println("Séance annulée : " + seance.toString());
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
         } else {
             System.out.println("Séance déja annulée.");
         }
@@ -119,20 +151,70 @@ public class MajDonnees {
     /**
      * Valider une séance (changer l’état de cette séance à « validée »).
      * @param seance Objet Seance a valider.
-     * @throws SQLException Probleme requete.
-     * @throws ClassNotFoundException Probleme driver.
      */
-    public void validerSeance(Seance seance) throws SQLException, ClassNotFoundException {
+    public void validerSeance(Seance seance){
         if (seance.getEtat() != 1) {
             seance.setEtat(1);
-            seanceDao.modifier(seance);
+            try {
+                seanceDao.modifier(seance);
+                System.out.println("Seance validée : " + seance.toString());
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
         } else {
             System.out.println("Seance déja validée.");
         }
     }
 
     /**
-     * Enlever un groupe ou/et un enseignant à une séance de cours
+     * Enlever un groupe d'une séance de cours.
+     * @param seance Seance a modifier.
+     * @param grp Groupe a supprimer de la séance.
      */
+    public void removeGroupeFromSeance(final Groupe grp, final Seance seance) {
+        Seance_Groupes sg = new Seance_Groupes();
+        sg.setGroupe(grp);
+        sg.setSeance(seance);
+        try {
+            sgDao.supprimer(sg);
+            System.out.println(grp.toString() + " supprimé de " + seance.toString());
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Enlever un enseignant d'une séance de cours.
+     * @param seance Seance a modifier.
+     * @param prof Enseignant a supprimer de la séance.
+     */
+    public void removeEnseignantFromSeance(final Enseignant prof, final Seance seance) {
+        Seance_Enseignants se = new Seance_Enseignants();
+        se.setProf(prof);
+        se.setSeance(seance);
+        try {
+            seDao.supprimer(se);
+            System.out.println(prof.toString() + " supprimé de " + seance.toString());
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Enlever une salle d'une séance de cours.
+     * @param seance Seance a modifier.
+     * @param salle Salle a supprimer de la séance.
+     */
+    public void removeSalleFromSeance(final Salle salle, final Seance seance) {
+        Seance_Salles ss = new Seance_Salles();
+        ss.setSalle(salle);
+        ss.setSeance(seance);
+        try {
+            ssDao.supprimer(ss);
+            System.out.println(salle.toString() + " supprimée de " + seance.toString());
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
 }
