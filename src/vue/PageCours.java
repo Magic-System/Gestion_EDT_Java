a41c0bc8830a2ad7684b0efd5287b0e0397028ab
Constantin <constantinkozlow@gmail.com>
2020-06-08 13:39:45 +0200

    Ajouts mais pas fonctionnel

--- src/vue/PageCours.java
+++ src/vue/PageCours.java
@@ -46,11 +46,12 @@ class PageCours extends JPanel implements ActionListener{
     //Panel EDT
     private JPanel panelEDTNord, panelEDTCenter, panImage;
     //Choix semaine
-    private JLabel labelSemaine;
-    private JComboBox comboSemaine;
+    private JLabel labelSemaine, labelChoixAffichage;
+    private JComboBox comboSemaine, comboChoixAffichage;
     private int semaineAct;
     private final int[] tabChoixSemaine = {31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
     private final String[] tabLabelsEDT = {"\nHoraires","\nLundi","\nMardi","\nMercredi","\nJeudi","\nVendredi","\nSamedi"};
+    private final String[] tabListeEDT = {"Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
     private final String[] tabCreneauxEDT = {"", "8h30\n\n\n10h", "10h15\n\n\n11h45", "12h\n\n\n13h30", "13h45\n\n\n15h15", "15h30\n\n\n17h", "17h15\n\n\n18h45", "19h\n\n\n20h30"};
     
     //Utilisateur connecté
@@ -89,10 +90,17 @@ class PageCours extends JPanel implements ActionListener{
         panelEDTNord.setLayout(new FlowLayout(FlowLayout.LEFT));
         panelEDTNord.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
         
+        //Initialisation choix affichage
+        labelChoixAffichage = new JLabel("Affichage");
+        comboChoixAffichage = new JComboBox();
+        comboChoixAffichage.setPreferredSize(new Dimension(60,25));
+        comboChoixAffichage.addItem("en grille");
+        comboChoixAffichage.addItem("en liste");
+        
         //Initialisation choix semaine
         labelSemaine = new JLabel("Choisissez une semaine :");
         comboSemaine = new JComboBox();
-        comboSemaine.setPreferredSize(new Dimension(60,25));
+        comboSemaine.setPreferredSize(new Dimension(100,25));
         for(int choix : tabChoixSemaine){
             comboSemaine.addItem(choix);
         }
@@ -107,6 +115,8 @@ class PageCours extends JPanel implements ActionListener{
         comboSemaine.setSelectedItem(semaineAct);
         
         //Ajouts au panel
+        //panelEDTNord.add(labelChoixAffichage);
+        //panelEDTNord.add(comboChoixAffichage);
         panelEDTNord.add(labelSemaine);
         panelEDTNord.add(comboSemaine);
     }
@@ -236,7 +246,7 @@ class PageCours extends JPanel implements ActionListener{
                             int joursSeance = maSemaine.get(k).getJour().getDayOfWeek().getValue();
                             if(joursSeance == numJours)
                             {
-                                //Numéro du créneau de la séance donné
+                                //Numéro du créneau de la séance donnée
                                 int numCreneauSeance = 0;
                                 //Récupération de l'heure de début de la séance donné
                                 LocalTime creneauSeance = maSemaine.get(k).getHeure_debut();
@@ -359,15 +369,6 @@ class PageCours extends JPanel implements ActionListener{
         JPanel panelCentre = new JPanel();
         panelCentre.setLayout(new GridLayout(7, 1));
         
-        //Initialisation des créneaux
-        LocalTime creneau1 = LocalTime.parse("08:30:00.0");
-        LocalTime creneau2 = LocalTime.parse("10:15:00.0");
-        LocalTime creneau3 = LocalTime.parse("12:00:00.0");
-        LocalTime creneau4 = LocalTime.parse("13:45:00.0");
-        LocalTime creneau5 = LocalTime.parse("15:30:00.0");
-        LocalTime creneau6 = LocalTime.parse("17:15:00.0");
-        LocalTime creneau7 = LocalTime.parse("19:00:00.0");
-        
         //Récupération des séances du user pour la semaine donné, en fonction du type d'utilisateur (étudiant / enseignant)
         ArrayList<Seance> maSemaine;
         
@@ -409,22 +410,88 @@ class PageCours extends JPanel implements ActionListener{
                 //Première ligne = "Titre"
                 if(numCreneau == 0){
                     //On défini le titre
-                    titre.setText(tabLabelsEDT[numJours]);
+                    titre.setText(tabListeEDT[numJours]);
                     panelCreneau.add(titre);
                 }
-                
-                
-                
-                
-                
+                else
+                {
+                    //On parcours la liste de séance Pour check si la séance donné correspond à la séance [i][j]
+                    for(int k=0; k<maSemaine.size(); k++)
+                    {
+                        //Check si le jours correspond
+                        int joursSeance = maSemaine.get(k).getJour().getDayOfWeek().getValue();
+                        if(joursSeance == numJours)
+                        {
+                            //Alors on récupère le reste des infos nécessaires
+                            int idSeance = maSemaine.get(k).getId();
+                            //Récupération de l'état de la séance
+                            int etatSeance = maSemaine.get(k).getEtat();
+                            //Récupération du nom de la séance (nom du cours)
+                            String nomSeance = maSemaine.get(k).getCours().getNom();
+                            //Récupération de la couleur de la séance
+                            Color couleurSeance;
+                            try {
+                                Field field = Class.forName("java.awt.Color").getField((String)maSemaine.get(k).getCours().getCouleur().toLowerCase());
+                                couleurSeance = (Color)field.get(null);
+                            } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException e) {
+                                //Couleur par défaut
+                                couleurSeance = Color.lightGray;
+                            }
+                            //Récupération du nom du/des prof qui assurent ce cours
+                            ArrayList<String> listeProfsSeance = donnees.getNomEnseignantSeance(idSeance);
+                            //Récupération du/des TD participant à la séance
+                            ArrayList<String> listeTDSeance = donnees.getNomGroupeSeance(idSeance);
+                            //Récupération de la salle de cours
+                            ArrayList<String > listeSallesSeance = donnees.getNomSalleSeance(idSeance);
+
+                            //Initialisation de la 'String' d'affichage
+                            String stringSeance = "";
+                            //Check si le cours est annulé
+                            if(etatSeance == 0){
+                                stringSeance += "-ANNULE- ";
+                            }
+                            //Ajout nom de la Séance
+                            stringSeance += nomSeance;
+                            stringSeance += " | ";
+                            //Si user == prof, ajout du/des TD qui participent
+                            if(user.getDroit() == 3){
+                                for(int l=0; l<listeTDSeance.size(); l++){
+                                    stringSeance += listeTDSeance.get(l);
+                                    if(listeTDSeance.size() > 1){
+                                        stringSeance += " - ";
+                                    }
+                                }
+                            }
+                            //Si user == étudiant, ajout du/des profs qui donnent le cours
+                            if(user.getDroit() == 4){
+                                stringSeance += "Mme|M. ";
+                                for(int l=0; l<listeProfsSeance.size(); l++){
+                                    stringSeance += listeProfsSeance.get(l);
+                                    if(listeProfsSeance.size() > 1){
+                                        stringSeance += " - ";
+                                    }
+                                }
+                            }
+                            stringSeance += " | ";
+                            //Ajout salles 
+                            for(int l=0; l<listeSallesSeance.size(); l++){
+                                stringSeance += listeSallesSeance.get(l);
+                                if(listeSallesSeance.size() > 1){
+                                    stringSeance += " - ";
+                                }
+                            }
+                            //Puis on rajoute dans la case
+                            creneau.setText(stringSeance);
+                            creneau.setBorder(BorderFactory.createMatteBorder(1, 5, 1, 1, couleurSeance));
+                            
+                        }
+                        panelCreneau.add(creneau);
+                    }
+                }
+                panelJours.add(panelCreneau);
             }          
-            
-        
+            panelCentre.add(panelJours);
         }
-        
-        
-        
-        
         return panelCentre;
     }
     
@@ -444,11 +511,27 @@ class PageCours extends JPanel implements ActionListener{
         {
             //On récupère le choix
             int semaineSelectionnee = (int)comboSemaine.getSelectedItem();
-            panelEDTCenter.removeAll();
-            panelEDTCenter.revalidate();
-            panelEDTCenter.repaint();
-            panelEDTCenter.add(panImage, BorderLayout.NORTH);
-            panelEDTCenter.add(dessinerEDT(semaineSelectionnee), BorderLayout.CENTER);
+            String typeAffichage = (String)comboChoixAffichage.getSelectedItem();
+            
+            //Si affichage en ligne :
+            /*if(typeAffichage.equals("en liste"))
+            {
+                System.out.println("LISTE");
+                panelEDTCenter.removeAll();
+                panelEDTCenter.revalidate();
+                panelEDTCenter.repaint();
+                panelEDTCenter.add(panImage, BorderLayout.NORTH);
+                panelEDTCenter.add(listeEDT(semaineSelectionnee), BorderLayout.CENTER); 
+            }
+            //Sinon affichage en grille :
+            else
+            {*/
+                panelEDTCenter.removeAll();
+                panelEDTCenter.revalidate();
+                panelEDTCenter.repaint();
+                panelEDTCenter.add(panImage, BorderLayout.NORTH);
+                panelEDTCenter.add(dessinerEDT(semaineSelectionnee), BorderLayout.CENTER);    
+            //}
         }
     }
 }
